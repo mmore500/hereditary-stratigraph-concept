0796dd33c89fc88e68bd67e7696fb6aa85fbd265
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-28 13:32:38 -0500

    Initial commit

---
+++ cpp/include/hstrat/example.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef HSTRAT_EXAMPLE_HPP_INCLUDE
+#define HSTRAT_EXAMPLE_HPP_INCLUDE
+
+#include <iostream>
+
+namespace hstrat {
+
+bool example() {
+  std::cout << "example thing!" << '\n';
+  return true;
+}
+
+} // namespace hstrat
+
+#endif // #ifndef HSTRAT_EXAMPLE_HPP_INCLUDE
