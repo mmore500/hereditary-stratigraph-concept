428f8e0f5d8506179ed046392332e994b855a50a
mmore500 <mmore500.login@gmail.com>
2023-07-15 22:34:42 +0000

    Deploying to binder from @ mmore500/hereditary-stratigraph-concept@a9b5c108d94d2b11544119ec6180b0c0944fc3f2 🚀

--- cpp/tests/hstrat/hstrat/HereditaryStratum.cpp
+++
@@ -1,8 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "hstrat/HereditaryStratum.hpp"
-
-TEST_CASE("Test HereditaryStratum") {
-  hstrat::HereditaryStratum{};
-  // TODO flesh out stub test
-}
--- cpp/tests/hstrat/stratum_retention_predicates/StratumRetentionPredicateMaximal.cpp
+++
@@ -1,8 +0,0 @@
-#include "Catch/single_include/catch2/catch.hpp"
-
-#include "hstrat/stratum_retention_predicates/StratumRetentionPredicateMaximal.hpp"
-
-TEST_CASE("Test StratumRetentionPredicateMaximal") {
-  hstrat::StratumRetentionPredicateMaximal{};
-  // TODO flesh out stub test
-}
